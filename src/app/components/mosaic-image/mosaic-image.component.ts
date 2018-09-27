7657bcdff3acc1c309734962f87fa832dbfb8e7c
dhatawesomedude <deb2fast@gmail.com>
2018-09-27 13:23:47 +1000

    refactor mosaic-photo-service --refactor mosaic-image component

--- src/app/components/mosaic-image/mosaic-image.component.ts
+++ src/app/components/mosaic-image/mosaic-image.component.ts
@@ -1,49 +1,49 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Event } from '@angular/router';
 import randomWords from 'random-words';
 import { finalize } from 'rxjs/operators';
 import { AppResource } from '../../services/app.resource';
 import { PhotoMosaicService } from '../../services/mosaic-photo.service';
 
-interface Color {
-  r: number;
-  g: number;
-  b: number;
-}
-
-
 @Component({
   selector: 'app-mosaic-image',
   templateUrl: './mosaic-image.component.html',
-  styleUrls: ['./mosaic-image.component.css']
+  styleUrls: ['./mosaic-image.component.css'],
+  viewProviders: [PhotoMosaicService]
 })
 
 export class MosaicImageComponent implements OnInit, AfterViewInit {
 
   @ViewChild('mosaicCanvas') readonly canvasRef: ElementRef;
-  @Input() tileShape: 'circle' | 'rect' = 'circle';
-  @Input() opacity = 1;
-  // TODO: complete
-  // image src can either be url or base64 | defaults to base64.
+
+  // image src can either be image-url passed in as a router param
+  // or base64 data Url
+  // Defaults to base64 data Url.
   imageUrl: string;
   @Input() base64ImageData: string;
+
+  // eventEmitter - emits on completion of mosaic
   @Output() mosaicGenerated = new EventEmitter<void>();
+
   public imageUploadClicked = false;
   public isLoading = false;
   public uploadError = false;
   public uploadedImageUrl: string;
-  private photoMosaicService: PhotoMosaicService;
 
-  constructor(private route: ActivatedRoute, private appResource: AppResource) {
-  }
+  constructor (private route: ActivatedRoute,
+               private appResource: AppResource,
+               private photoMosaicService: PhotoMosaicService) {}
+
   public ngOnInit (): void {
     // get image-url from url params.
     if (!this.base64ImageData) {
       this.route.params.subscribe(params => this.imageUrl = params.id);
     }
   }
+
   public ngAfterViewInit (): void {
-    this.photoMosaicService = new PhotoMosaicService({
+    this.photoMosaicService
+      .setupMosaic({
       imageUrl: this.imageUrl || this.base64ImageData,
       iWidth: 500,
       iHeight: 500,
@@ -52,22 +52,24 @@ export class MosaicImageComponent implements OnInit, AfterViewInit {
       noOfVerticalTiles: 1,
       noOfHorizontalTiles: 1,
       canvasRef: this.canvasRef
-    });
-    this.photoMosaicService.setupMosaic();
-    this.photoMosaicService.onComplete.subscribe((event) => this.mosaicGenerated.emit());
+    })
+      .subscribe(() => this.mosaicGenerated.emit());
   }
+
   public convertCanvasToImage (): void {
     this.imageUploadClicked = true;
     this.isLoading = true;
+
     const image = new Image();
     image.crossOrigin = 'Anonymous';
     image.src = this.canvasRef.nativeElement.toDataURL();
     const imageTitle = randomWords(3).join(' ');
-    this.appResource.uploadImage(imageTitle, image.src.replace(/^data:image\/(png|jpg);base64,/, ''))
+
+    this.appResource.uploadImage (imageTitle, image.src.replace(/^data:image\/(png|jpg);base64,/, ''))
       .pipe(finalize(() => this.isLoading = false))
       .subscribe((res) => {
         this.uploadError = false;
         this.uploadedImageUrl = res.data.link;
-        }, error => this.uploadError = true);
+      }, error => this.uploadError = true);
   }
 }
--- src/app/services/mosaic-photo.service.ts
+++ src/app/services/mosaic-photo.service.ts
@@ -1,20 +1,24 @@
-import { ElementRef, EventEmitter, Injectable, Renderer2 } from '@angular/core';
+import { ElementRef, EventEmitter, Injectable } from '@angular/core';
 import { app } from '../models/app.model';
 
 @Injectable()
 export class PhotoMosaicService {
+  public readonly onComplete = new EventEmitter<void>();
+
   private iWidth = 500;
   private iHeight = 500;
-  private readonly tileHeight: number;
-  private readonly tileWidth: number;
+  private tileHeight: number;
+  private tileWidth: number;
   private noOfVerticalTiles = 1;
   private noOfHorizontalTiles = 1;
-  private readonly imageUrl: string;
-  public readonly onComplete = new EventEmitter<void>();
+  private imageUrl: string;
   private canvasRef: ElementRef;
-  private tileShape = app.TileShape.circle;
-  private readonly opacity: number = 1;
-  constructor (options: app.MosaicServiceOptions) {
+  private readonly tileShape = app.TileShape.circle;
+  private opacity = 1;
+
+  constructor () {}
+
+  public setupMosaic(options: app.MosaicServiceOptions): EventEmitter<void> {
     this.imageUrl = options.imageUrl;
     this.iWidth = options.iWidth;
     this.iHeight = options.iHeight;
@@ -24,10 +28,10 @@ export class PhotoMosaicService {
     this.noOfVerticalTiles = options.noOfVerticalTiles;
     this.canvasRef = options.canvasRef;
     this.opacity = options.opacity || this.opacity;
-  }
-  public setupMosaic(): void {
     this.drawImage((context: CanvasRenderingContext2D) => this.tileCanvas(context));
+    return this.onComplete;
   }
+
   private drawImage (done: Function): void {
     const ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
     ctx.fillStyle = 'rgba(0, 0, 0, 0)';
@@ -35,23 +39,26 @@ export class PhotoMosaicService {
     ctx.rect(0, 0, this.iWidth, this.iHeight);
     ctx.closePath();
     ctx.fill();
+
+    // draw image on HTMLCanvasElement
     const image = new Image();
     image.onload = () => {
-      this.resize(image.height, image.width);
+      this.resize();
       ctx.drawImage(image, 0, 0, this.iWidth, this.iHeight);
       done(ctx);
     };
     image.crossOrigin = 'Anonymous';
     image.src = this.imageUrl;
   }
-  private resize (height: number, width: number): void {
-    this.canvasRef.nativeElement.height = 500;
-    this.canvasRef.nativeElement.width = 500;
-    this.iWidth = 500;
-    this.iHeight = 500;
+
+  // set canvas height and width
+  private resize (): void {
+    this.canvasRef.nativeElement.height = this.iHeight;
+    this.canvasRef.nativeElement.width = this.iWidth;
     this.noOfHorizontalTiles = Math.floor(this.iWidth / this.tileWidth);
     this.noOfVerticalTiles = Math.floor(this.iHeight / this.tileHeight);
   }
+
   // Returns the average color of the canvas.
   private findAverageColour (data: any[]): app.Color {
     let i = -4, count = 0;
@@ -77,26 +84,32 @@ export class PhotoMosaicService {
 
     return rgb;
   }
+
   // Divides the whole canvas into smaller tiles and finds the average
   // colour of each block. After calculating the average colour, it stores
   // the data into an array.
   private tileCanvas (context: CanvasRenderingContext2D): void {
-    const originalImageData: ImageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
+    const originalImageData: ImageData =
+      context.getImageData(0, 0, context.canvas.width, context.canvas.height);
+
     for (let i = 0; i < this.noOfVerticalTiles; i++) {
       for (let j = 0; j < this.noOfHorizontalTiles; j++) {
         const x = j * this.tileWidth, y = i * this.tileHeight;
         const imageData = this.getImageData(x, y, this.iWidth, originalImageData);
         const averageColor = this.findAverageColour(imageData);
-        const color = 'rgba(' + averageColor.r + ',' + averageColor.g + ',' + averageColor.b + ',' + this.opacity + ')';
-        context.fillStyle = color;
+        context.fillStyle = `rgba( ${averageColor.r},${averageColor.g},${averageColor.b},${this.opacity})`;
         this.createMosaic(x, y, context);
       }
     }
     this.onComplete.emit();
   }
+
   // Creates an array of the image data of the tile from the data of whole image
-  private getImageData (startX: number, startY: number, width: number, originalImageData: ImageData): number[] {
-    const data = [];
+  private getImageData (startX: number,
+                        startY: number,
+                        width: number,
+                        originalImageData: ImageData): number[] {
+    const data: number[] = [];
     for (let x = startX; x < (startX + this.tileWidth); x++) {
       const xPos = x * 4;
       for (let y = startY; y < (startY + this.tileHeight); y++) {
@@ -111,8 +124,12 @@ export class PhotoMosaicService {
     }
     return data;
   }
-  // Creates a block of the mosaic. This is called (noOfVerticalTiles * noOfVerticalTiles)  times to create all blocks
-  private createMosaic (x: number, y: number, context: CanvasRenderingContext2D): void {
+
+  // Creates a block of the mosaic.
+  // This is called (noOfVerticalTiles * noOfVerticalTiles)  times to create all blocks
+  private createMosaic (x: number,
+                        y: number,
+                        context: CanvasRenderingContext2D): void {
     if (this.tileShape === app.TileShape.circle) {
       const centerX = x + this.tileWidth / 2;
       const centerY = y + this.tileHeight / 2;
