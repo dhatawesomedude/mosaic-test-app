3900fdd90d59a49439d1da205798c2b647b01d9c
dhatawesomedude <deb2fast@gmail.com>
2018-09-28 12:40:20 +1000

    remove unused code

--- src/app/components/custom-image-selector/custom-image-selector.component.spec.ts
+++ src/app/components/custom-image-selector/custom-image-selector.component.spec.ts
@@ -1,28 +1,17 @@
 import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { MatProgressSpinnerModule } from '@angular/material';
 import { By } from '@angular/platform-browser';
-import { of } from 'rxjs';
-import { imageListMock } from '../../mocks/mocks';
 import { CustomImageSelectorComponent } from './custom-image-selector.component';
 
 let fixture: ComponentFixture<CustomImageSelectorComponent>;
 let component: CustomImageSelectorComponent;
 let componentDE: DebugElement;
-let routerStub: any;
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
 
-    // routerStub = jasmine.createSpy('params');
-    routerStub = {
-      params: of({id: imageListMock[1].link})
-    };
-
     TestBed.configureTestingModule({
-      imports: [
-        MatProgressSpinnerModule
-      ],
+      imports: [],
       declarations: [
         CustomImageSelectorComponent
       ],
